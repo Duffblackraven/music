41ce65fc026c3a0b9552f7d1a3c6fb00e29820a3
duffblackraven <t9164700668@gmail.com>
2024-04-12 13:28:04 +0200

    hw5_new почти как надо

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3778507011510ff1"; }
+/******/ 		__webpack_require__.h = function() { return "fb074bbd7bcf1100"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
